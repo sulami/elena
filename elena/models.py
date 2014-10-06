b88b75e822d4804d365a5d7a021fdfcc6510d453
Robin Schroer <sulamiification@gmail.com>
2014-10-06 20:27:27 +0200

    add some basic pass models

---
+++ elena/models.py
@@ -0,0 +1,20 @@
+class Status:
+    """Base status class to inherit from"""
+    pass
+
+class BooleanStatus(Status):
+    """Expresses a boolean status, e.g. up or down"""
+    pass
+
+class NumberStatus(Status):
+    """Expresses a whole number status, e.g. number of pageviews"""
+    pass
+
+class FloatStatus(Status):
+    """Expresses a floating point status, e.g. a response time"""
+    pass
+
+class StringStatus(Status):
+    """Expresses a string status, which can be anything"""
+    pass
+
